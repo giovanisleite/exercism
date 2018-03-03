4c0ba4f681ea3a53cd87079fbdf8fdf3f249e5e3
Giovani <sousa_giovani@hotmail.com>
2018-03-03 17:58:20 -0300

    Add isbn verifier solution

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,10 @@
 def verify(isbn):
-    pass
+    try:
+        clean_isbn = isbn.replace('-', '')
+        total_sum = 0
+        for index, digit in enumerate(clean_isbn[:-1]):
+            total_sum += (10-index) * int(digit)
+        total_sum += 10 if clean_isbn[-1] == 'X' else int(clean_isbn[-1])
+        return total_sum % 11 == 0
+    except:
+        return False
